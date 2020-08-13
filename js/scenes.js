d51d146057daeaba80a0daa081683d919963e116
Cat Nguyen <ctn57d@gmail.com>
2020-08-13 12:26:56 -0400

    Added main menu buttons

--- js/scenes.js
+++ js/scenes.js
@@ -17,28 +17,41 @@ var MenuScene = new Phaser.Class({
 
     preload: function ()
     {
-        this.load.image('menu_background', 'assets/minecraftMenuBackground.jpg')
+        this.load.json('menu', 'assets/menu.json')
+        this.load.image('menu_background', 'assets/minecraftMenuBackground.jpg');
+        this.load.image('story_mode_button', 'assets/StoryModeButton.png');
+        this.load.image('versus_mode_button', 'assets/VersusModeButton.png');
     },
 
     create: function ()
     {
+        let data = this.cache.json.get('menu');
         //console.log(this);  -- Debugging purposes
-        this.add.image(0,0,'menu_background').setOrigin(0,0)
+        let background = this.add.image(data.background.x, data.background.y, data.background.image);
+        let storyButton = this.add.image(data.button1.x, data.button1.y, data.button1.image);
+        let versusButton = this.add.image(data.button2.x, data.button2.y, data.button2.image);
         
         // Temporary instruction
-        let scoreText = this.add.text(16,16,'Press <Space> to Start...', {fontSize: '32px', fill: '#FFF' });
-        
-        // WILL UPDATE THIS TO BE MOUSE POINT AND CLICK
-        cursors = this.input.keyboard.createCursorKeys();
-    },
+        let scoreText = this.add.text(225,580,'Click on Story Mode to Start!', {fontSize: '20px', fill: '#FFF' });
+        let message = this.add.text(430,450, "Under Construction!", {fontSize: '30px', fill: '#FFF'})
+        message.setVisible(false);
 
-    update: function()
-    {
-        if (cursors.space.isDown)
-        {
-            this.scene.start('easylevelscene');  // Transitions to the next scene
-        }
-    }
+        storyButton.setInteractive();
+        storyButton.on("pointerup", () => {
+            message.setVisible(false);
+            this.scene.start('easylevelscene');
+        })
+        versusButton.setInteractive();
+        versusButton.on("pointerup", () => {
+            //scoreText.setText('Currently under construction!');
+            message.setVisible(true);
+            console.log("Next menu in progress!");
+        })
+        background.setInteractive();
+        background.on("pointerup", () => {
+            message.setVisible(false);
+        })
+    },
 });
 
 var EasyLevelScene = new Phaser.Class({
@@ -87,14 +100,12 @@ var EasyLevelScene = new Phaser.Class({
 
     create: function ()
     {
-        let platforms;
-
         let data = this.cache.json.get('easy-level');
         this.add.image(0,0,data.backgroundImage).setOrigin(0,0)
         console.log("Onto the next scene!");
 
         // Platforms group is a grouping for all ground objects
-        platforms = this.physics.add.staticGroup();  // static object never moves
+        let platforms = this.physics.add.staticGroup();  // static object never moves
         
         let groundData = data.ground;
         let platformData = data.platforms;
