118fa29c0baba81ed8d3c6508be83ad58e0bf840
Cat Nguyen <ctn57d@gmail.com>
2020-09-03 21:07:30 -0400

    Added model face to to indicate what model the user is using (Click on it to switch between guy and girl)

--- js/scenes/main-menu.js
+++ js/scenes/main-menu.js
@@ -15,7 +15,9 @@ var MainMenu = new Phaser.Class({
         this.load.json('menu', 'json/menu.json')
         this.load.image('menu_background', 'assets/minecraftMenuBackground.jpg');
         this.load.image('story_button', 'assets/StoryModeButton.png');
-        this.load.image('infinite_button', 'assets/InfiniteMode.png')
+        this.load.image('infinite_button', 'assets/InfiniteMode.png');
+        this.load.image('girl_face', 'assets/girl_face.png');
+        this.load.image('guy_face', 'assets/guy_face.png');
     },
 
     create: function ()
@@ -25,13 +27,15 @@ var MainMenu = new Phaser.Class({
         this.add.image(data.background.x, data.background.y, data.background.image);
         let storyButton = this.add.image(data.button1.x, data.button1.y, data.button1.image);
         let infiniteButton = this.add.image(data.button2.x, data.button2.y, data.button2.image);
+        let girlButton = this.add.image(data.girlFace.x, data.girlFace.y, data.girlFace.image);
+        let guyButton = this.add.image(data.guyFace.x, data.guyFace.y, data.guyFace.image);
 
         score = 0;
         currentLevel = null;
         returnMenu = false;
 
         // Temporary instruction   
-        this.add.text(275,570,'Click Any Mode to Start!', {fontSize: '20px', fill: '#FFF' });
+        this.add.text(260,570,'Click Any Mode to Start!', {fontSize: '20px', fill: '#FFF' });
 
         storyButton.setInteractive();
         storyButton.on("pointerup", () => {
@@ -42,5 +46,24 @@ var MainMenu = new Phaser.Class({
         infiniteButton.on("pointerup", () => {
             this.scene.start('generatedlevelscene');
         })
+
+        guyButton.setVisible(false);
+
+        girlButton.setInteractive();
+        girlButton.on("pointerup", () => {
+            playerData.name = 'guy';
+            girlButton.setVisible(false);
+            guyButton.setVisible(true);
+            console.log("You are now the " + playerData.name);
+        })
+
+        guyButton.setInteractive();
+        guyButton.on("pointerup", () => {
+            playerData.name = 'girl';
+            guyButton.setVisible(false);
+            girlButton.setVisible(true);
+            console.log("You are now the " + playerData.name);
+        })
+
     },
 });
